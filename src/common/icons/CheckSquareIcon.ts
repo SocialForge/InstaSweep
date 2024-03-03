0f24086cec4e533007bdd0c8f4de6e7625751383
SocialForge <socialforge@proton.me>
2024-03-04 00:11:07 +0200

    common/icons: avoid misusing .tsx
    No JSX is present in any of these files. No reason to use the TSX
    file extension.

--- src/common/icons/CheckSquareIcon.ts
+++ src/common/icons/CheckSquareIcon.ts
@@ -1,4 +1,4 @@
-import { createIcon } from '../../common/icon';
+import { createIcon } from '../icon';
 import checkSquareIcon from '../../assets/svg/check-square.svg';
 
 export const CheckSquareIcon = createIcon(checkSquareIcon);
--- src/common/icons/LockIcon.ts
+++ src/common/icons/LockIcon.ts
@@ -1,4 +1,4 @@
-import { createIcon } from '../../common/icon';
+import { createIcon } from '../icon';
 import lockIcon from '../../assets/svg/lock.svg';
 
 export const LockIcon = createIcon(lockIcon);
